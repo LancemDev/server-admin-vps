c17ca2bf17381d9cbbfc5b5df07cd4825eacd900
LancemDev <munyaolance1@gmail.com>
2024-10-08 18:03:06 +0300

    :construction: live site change

--- index.php
+++ index.php
@@ -8,15 +8,17 @@
 <body>
     Gatanna, Mark, Arnold and Lance
 
+    <br />
     If you're seeing this its too early to celebrate. We are still working on the server. 
     We will let you know when we are done.
 
+    <br />
 
     new changes
-
+    <br />
 
     Newest
-
+    <br />
 
     This is the latest change
 </body>
